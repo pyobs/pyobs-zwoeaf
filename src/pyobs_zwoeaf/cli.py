d5c3e62d0480d606bd885078d231b9588ee1d615
Tim-Oliver Husser <thusser@uni-goettingen.de>
2025-08-03 16:51:26 +0200

    started cli

---
+++ src/pyobs_zwoeaf/cli.py
@@ -0,0 +1,54 @@
+class EAF:
+    def Connect(self):
+        pass
+
+    def GetMaximalStep(self):
+        pass
+
+    def SetMaximalStep(self):
+        pass
+
+    def GetSound(self):
+        pass
+
+    def SetSound(self):
+        pass
+
+    def Temperature(self):
+        pass
+
+    def Property(self):
+        pass
+
+    def Moving(self):
+        pass
+
+    def GetPosition(self):
+        pass
+
+    def SetPosition(self):
+        pass
+
+    def GetDirection(self):
+        pass
+
+    def SetDirection(self):
+        pass
+
+    def MoveRange(self):
+        pass
+
+    def MoveToPosition(self):
+        pass
+
+    def MoveStop(self):
+        pass
+
+    def GetBacklash(self):
+        pass
+
+    def SetBacklash(self):
+        pass
+
+    def Disconnect(self):
+        pass
